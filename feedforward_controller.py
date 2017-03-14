14ded09417bb6b122efabb5c434ef70ae50eb9ee
camigord <camigord@gmail.com>
2017-03-14 18:14:52 +0100

    Simplifying the model. 1 FC layer with 100 neurons

--- feedforward_controller.py
+++ feedforward_controller.py
@@ -13,19 +13,19 @@ class FeedforwardController(BaseController):
         initial_std = lambda in_nodes: np.min([1e-2, np.sqrt(2.0 / in_nodes)])
         input_ = self.nn_input_size
 
-        self.W1 = tf.Variable(tf.truncated_normal([input_, 128], stddev=initial_std(input_)), name='layer1_W')
-        self.W2 = tf.Variable(tf.truncated_normal([128, 256], stddev=initial_std(128)), name='layer2_W')
-        self.b1 = tf.Variable(tf.zeros([128]), name='layer1_b')
-        self.b2 = tf.Variable(tf.zeros([256]), name='layer2_b')
+        self.W1 = tf.Variable(tf.truncated_normal([input_, 100], stddev=initial_std(input_)), name='layer1_W')
+        #self.W2 = tf.Variable(tf.truncated_normal([128, 256], stddev=initial_std(128)), name='layer2_W')
+        self.b1 = tf.Variable(tf.zeros([100]), name='layer1_b')
+        #self.b2 = tf.Variable(tf.zeros([256]), name='layer2_b')
 
     def network_op(self, X):
         l1_output = tf.matmul(X, self.W1) + self.b1
         l1_activation = tf.nn.relu(l1_output)
 
-        l2_output = tf.matmul(l1_activation, self.W2) + self.b2
-        l2_activation = tf.nn.relu(l2_output)
+        #l2_output = tf.matmul(l1_activation, self.W2) + self.b2
+        #l2_activation = tf.nn.relu(l2_output)
 
-        return l2_activation
+        return l1_activation
 
     def initials(self):
         initial_std = lambda in_nodes: np.min([1e-2, np.sqrt(2.0 / in_nodes)])
